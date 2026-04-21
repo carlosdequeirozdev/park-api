5821da3025d92e6278bd66476a33e2debb8b38ca
Carlos de Queiroz <carlos.dequeiroz.dev@gmail.com>
2026-04-21 09:47:22 -0400

    Tratamento de excecao lancada no processo de validacao de campos

---
+++ src/main/java/com/queirozdec/park_api/web/exception/ApiExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.queirozdec.park_api.web.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+@Slf4j
+@RestControllerAdvice
+public class ApiExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessage> methodArgumentNotValidException(MethodArgumentNotValidException ex,
+                                                                        HttpServletRequest request,
+                                                                        BindingResult result) {
+        log.error("API error - ", ex);
+        return ResponseEntity
+                .status(HttpStatus.UNPROCESSABLE_CONTENT)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.UNPROCESSABLE_CONTENT, "Campo(s) inválido(s).", result));
+
+    }
+}
